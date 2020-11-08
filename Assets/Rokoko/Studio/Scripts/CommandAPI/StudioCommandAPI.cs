fd9589a2f44d88e41e0db98c6d4259c51fb658c5
rasm795h <razzz.mus@gmail.com>
2020-11-08 19:37:55 +0100

    updated to 2020

--- Assets/Rokoko/Studio/Scripts/CommandAPI/StudioCommandAPI.cs
+++
@@ -1,9 +0,0 @@
-namespace Rokoko.CommandAPI
-{
-    public class StudioCommandAPI : StudioCommandAPIBase
-    {
-        public string ipAddress;
-        protected override string IP => ipAddress;
-        protected override RequestData GetRequestData() => new RequestData();
-    }
-}
